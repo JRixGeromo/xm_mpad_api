f9e74a2135825910a9fd2a72839a6c6ddcdec415
jrixgeromo <jrixgeromo@gmail.com>
2022-02-09 20:54:24 +0800

    updates

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,45 +1,45 @@
 <template>
-    <div id="filter-bar" @click="showHideFilter">Filter <i class="xm-el-icon-plus"></i>
-        <div id="filter-div" style="display: none;">
-        <el-row style="text-align: cetner; margin-bottom: 2em;">
-            <el-col :span="24">
-            <p style="font-weight: bold; margin-bottom: 0px; margin-top: 0px">Licensors</p>
-            </el-col>
-            <el-col :span="12" :xs="24">
-            <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
-            </el-col>
-            <el-col :span="12" :xs="24">
-            <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
-            </el-col>
-            <el-col :span="12" :xs="24">
-            <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
-            </el-col>
-            <el-col :span="12" :xs="24">
-            <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
-            </el-col>
-            <el-col :span="12" :xs="24">
-            <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
-            </el-col>
-            <el-col :span="12" :xs="24">
-            <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
-            </el-col>
-        </el-row>
-        <el-row style="text-align: cetner; margin-bottom: 2em;">
-            <el-col :span="24">
-            <p style="font-weight: bold; margin-bottom: 0px">Status</p>
-            </el-col>
-            <el-col :span="24">
-            <el-radio v-model="radio1" label="1" size="large">All</el-radio>
-            </el-col>
-            <el-col :span="24">
-            <el-radio v-model="radio1" label="1" size="large">Pending</el-radio>
-            </el-col>
-            <el-col :span="24">
-            <el-radio v-model="radio1" label="1" size="large">Completed</el-radio>
-            </el-col>
-        </el-row>
-        </div>
+  <div id="filter-bar" @click="showHideFilter">Filter <i class="xm-el-icon-plus"></i>
+    <div id="filter-div" style="display: none;">
+      <el-row style="text-align: cetner; margin-bottom: 2em;">
+          <el-col :span="24">
+          <p style="font-weight: bold; margin-bottom: 0px; margin-top: 0px">Licensors</p>
+          </el-col>
+          <el-col :span="12" :xs="24">
+          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          </el-col>
+          <el-col :span="12" :xs="24">
+          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          </el-col>
+          <el-col :span="12" :xs="24">
+          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          </el-col>
+          <el-col :span="12" :xs="24">
+          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          </el-col>
+          <el-col :span="12" :xs="24">
+          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          </el-col>
+          <el-col :span="12" :xs="24">
+          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          </el-col>
+      </el-row>
+      <el-row style="text-align: cetner; margin-bottom: 2em;">
+          <el-col :span="24">
+          <p style="font-weight: bold; margin-bottom: 0px">Status</p>
+          </el-col>
+          <el-col :span="24">
+          <el-radio v-model="radio1" label="1" size="large">All</el-radio>
+          </el-col>
+          <el-col :span="24">
+          <el-radio v-model="radio1" label="1" size="large">Pending</el-radio>
+          </el-col>
+          <el-col :span="24">
+          <el-radio v-model="radio1" label="1" size="large">Completed</el-radio>
+          </el-col>
+      </el-row>
     </div>
+  </div>
 </template>
 <script>
 import { showHideFilterMenu } from '@/helpers';
@@ -89,6 +89,6 @@ export default {
       background-color:#fff;
       z-index: 1000;
       padding: 0px 20px 20px 20px;
-      max-width: 400px;
+      max-width: 300px;
   }
 </style>
