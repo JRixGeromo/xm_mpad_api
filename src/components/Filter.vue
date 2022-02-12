0a1928befea6226630c7380bcf8bcc97056ba5d2
djlemmor <lemmornuique@gmail.com>
2022-02-12 10:29:29 +0800

    Check and Fix Filter Dropdown (color, size, font)

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,41 +1,44 @@
 <template>
-  <div id="filter-bar" @click="showHideFilter">Filter <i class="xm-el-icon-plus"></i>
-    <div id="filter-div" style="display: none;">
-      <el-row style="text-align: cetner; margin-bottom: 2em;">
+  <div id="filter-bar" @click="showHideFilter" class="fs-20">Filter <i class="xm-el-icon-plus"></i>
+    <div id="filter-div" style="display: none;" class="font-m">
+      <el-row>
           <el-col :span="24">
-          <p style="font-weight: bold; margin-bottom: 0px; margin-top: 0px">Licensors</p>
+          <p style="font-weight: bold; margin-bottom: 0px; margin-top: 0px" class="fs-16">Licensors</p>
           </el-col>
-          <el-col :span="12" :xs="24">
-          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          <el-col :span="12" :xs="24" class="py-4">
+          <el-checkbox v-model="checked1" label="DC" size="large"></el-checkbox>
           </el-col>
-          <el-col :span="12" :xs="24">
-          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          <el-col :span="12" :xs="24" class="py-4">
+          <el-checkbox v-model="checked1" label="Disney" size="large"></el-checkbox>
           </el-col>
-          <el-col :span="12" :xs="24">
-          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          <el-col :span="12" :xs="24" class="py-4">
+          <el-checkbox v-model="checked1" label="Hasbro" size="large"></el-checkbox>
           </el-col>
-          <el-col :span="12" :xs="24">
-          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          <el-col :span="12" :xs="24" class="py-4">
+          <el-checkbox v-model="checked1" label="Marvel" size="large"></el-checkbox>
           </el-col>
-          <el-col :span="12" :xs="24">
-          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          <el-col :span="12" :xs="24" class="py-4">
+          <el-checkbox v-model="checked1" label="Star Wars" size="large"></el-checkbox>
           </el-col>
-          <el-col :span="12" :xs="24">
-          <el-checkbox v-model="checked1" label="Option 1" size="large"></el-checkbox>
+          <el-col :span="12" :xs="24" class="py-4">
+          <el-checkbox v-model="checked1" label="Warner Bros" size="large"></el-checkbox>
+          </el-col>
+          <el-col :span="12" :xs="24" class="py-4">
+          <el-checkbox v-model="checked1" label="Others" size="large"></el-checkbox>
           </el-col>
       </el-row>
-      <el-row style="text-align: cetner; margin-bottom: 2em;">
+      <el-row>
           <el-col :span="24">
-          <p style="font-weight: bold; margin-bottom: 0px">Status</p>
+          <p style="font-weight: bold; margin-bottom: 0px" class="fs-16">Status</p>
           </el-col>
-          <el-col :span="24">
-          <el-radio v-model="radio1" label="1" size="large">All</el-radio>
+          <el-col :span="24" class="py-4">
+          <el-radio v-model="radio1" label="1" size="large" style="color:#000;">All</el-radio>
           </el-col>
-          <el-col :span="24">
-          <el-radio v-model="radio1" label="1" size="large">Pending</el-radio>
+          <el-col :span="24" class="py-4">
+          <el-radio v-model="radio1" label="1" size="large" style="color:#000;">Pending</el-radio>
           </el-col>
-          <el-col :span="24">
-          <el-radio v-model="radio1" label="1" size="large">Completed</el-radio>
+          <el-col :span="24" class="py-4">
+          <el-radio v-model="radio1" label="1" size="large" style="color:#000;">Completed</el-radio>
           </el-col>
       </el-row>
     </div>
@@ -88,7 +91,8 @@ export default {
       left: 0px;
       background-color:#fff;
       z-index: 1000;
-      padding: 0px 20px 20px 20px;
+      padding: 20px;
       max-width: 300px;
+      box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   }
 </style>
